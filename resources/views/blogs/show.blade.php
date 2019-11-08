b697487214dd8ea188b411b28093b9dadcf6d757
ncs12 <krishtina@thulo.com>
2019-11-08 09:50:08 +0545

    adding some css

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -10,10 +10,10 @@
                         <a class="btn btn-primary btn-xs pull-left btn-margin-right"
                            href="{{route('blog.edit',$blog->id)}}">Edit</a>
 
-                        <form action="{{route('blog.delete',$blog->id)}}" method="post">
+                        <form  action="{{route('blog.delete',$blog->id)}}" class="blog-del" method="post">
                             {{method_field('delete')}}
                             {{csrf_field()}}
-                            <button type="submit" class="btn btn-danger btn-xs pull-left">Delete</button>
+                            <button type="submit" class="btn btn-danger btn-xs pull-left btn-margin-right btn-margin-right">Delete</button>
                         </form>
                     </div>
                 </div>
